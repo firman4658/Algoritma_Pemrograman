b65ca22bc53d872e22ac9d1cb7f407a82bdb4962
firman4658 <78971161+firman4658@users.noreply.github.com>
2022-01-30 22:31:01 +0800

    register

---
+++ app/src/main/java/com/example/algoritma_pemrograman/registrasi.kt
@@ -0,0 +1,11 @@
+package com.example.algoritma_pemrograman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registrasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrasi)
+    }
+}
