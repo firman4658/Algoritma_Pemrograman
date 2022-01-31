28e557cb3eef287c34b681a3683d60d8d65bd718
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2022-01-31 20:25:36 +0800

    xbf

--- app/src/main/java/com/example/algoritma_pemrograman/registrasi.kt
+++ app/src/main/java/com/example/algoritma_pemrograman/registrasi.kt
@@ -2,10 +2,20 @@ package com.example.algoritma_pemrograman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class registrasi : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrasi)
+
+
+// ...
+// Initialize Firebase Auth
+        auth = Firebase.auth
     }
 }
