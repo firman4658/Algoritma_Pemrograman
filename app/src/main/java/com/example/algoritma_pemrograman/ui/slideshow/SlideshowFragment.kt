77902e8198f07c584f7cf080dc200765f1008384
firman4658 <78971161+firman4658@users.noreply.github.com>
2022-02-03 00:23:45 +0800

    modul 3

--- app/src/main/java/com/example/algoritma_pemrograman/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/algoritma_pemrograman/ui/slideshow/SlideshowFragment.kt
@@ -30,10 +30,6 @@ private var _binding: FragmentPerulanganBinding? = null
     _binding = FragmentPerulanganBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textSlideshow
-    slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
     return root
   }
 
