bcea9248eeef0e414c6cc0263f58c27e59a96cfe
firman4658 <78971161+firman4658@users.noreply.github.com>
2022-02-03 21:14:25 +0800

    modul 2

--- app/src/main/java/com/example/algoritma_pemrograman/Menu.kt
+++ app/src/main/java/com/example/algoritma_pemrograman/Menu.kt
@@ -25,10 +25,7 @@ private lateinit var binding: ActivityMenuBinding
 
         setSupportActionBar(binding.appBarMenu.toolbar)
 
-        binding.appBarMenu.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_menu)
